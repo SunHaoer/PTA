e2af2326f61843ab16bfb5acf7e49c9c87579763
SunHaoer <2457302636@qq.com>
2018-08-02 08:43:09 +0800

    www.wochi.xin

--- PAT (Advanced Level) Practice/1114.cpp
+++ PAT (Advanced Level) Practice/1114.cpp
@@ -1,28 +1,31 @@
 #include<cstdio>
 #include<algorithm>
 using namespace std;
-const int MAX = 100010;
-bool visited[MAX];
+const int MAX = 10010;
+bool exist[MAX];
+int father[MAX];
+int cnt = 0;
 
-struct Data {       // 统计家庭的数据
-    int id, fid, mid, num, area;
-    int cid[10];
+struct Data {       // 录入的数据
+    int id, fid, mid, k;
+    int cid[6];
+    double num, area;
 } data[MAX];
 
-struct Node {       // 最终家庭的数据
-    int id, people;
+struct Family {
+    int id, k;
     double num, area;
-    bool flag;
-} ans[MAX];
+    double avgNum, avgArea;
+    bool visited;
+} family[MAX];
 
-int father[MAX];
-void initFather() {
+void init() {
     for(int i = 0; i < MAX; i++) {
         father[i] = i;
     }
 }
 
-int findFather(int x) {     // 寻找祖宗
+int findFather(int x) {
     int a = x;
     while(x != father[x]) {
         x = father[x];
@@ -35,18 +38,18 @@ int findFather(int x) {     // 寻找祖宗
     return x;
 }
 
-
-void Union(int a, int b) {      // 合并
+void Union(int a, int b) {
     int faA = findFather(a);
     int faB = findFather(b);
-    if(faA > faB) {             // 向编号小的合并
-        father[faA] = faB;
-    } else if(faA < faB) {
+    if(faA < faB) {
         father[faB] = faA;
+    }
+    if(faB < faA) {
+        father[faA] = faB;
     }
 }
 
-bool cmp(Node a, Node b) {
+bool cmp1(Family a, Family b) {
     if(a.area != b.area) {
         return a.area > b.area;
     } else {
@@ -54,57 +57,61 @@ bool cmp(Node a, Node b) {
     }
 }
 
+bool cmp2(Family a, Family b) {
+    if(a.avgArea != b.avgArea) {
+        return a.avgArea > b.avgArea;
+    } else {
+        return a.id < b.id;
+    }
+}
+
 int main()
 {
-    initFather();
-    int n, k, cnt = 0;
+    init();
+    int n;       // 总家庭数n, 家庭孩子数k
     scanf("%d", &n);
-    for(int i = 0; i < n; i++) {            // 输入信息
-        scanf("%d %d %d %d", &data[i].id, &data[i].fid, &data[i].mid, &k);
-        visited[data[i].id] = true;
+    for(int i = 0; i < n; i++) {        // 输入数据, 合并集合
+        scanf("%d %d %d %d", &data[i].id, &data[i].fid , &data[i].mid, &data[i].k);
+        exist[data[i].id] = true;
         if(data[i].fid != -1) {
-            visited[data[i].fid] = true;
             Union(data[i].id, data[i].fid);
+            exist[data[i].fid] = true;
         }
         if(data[i].mid != -1) {
-            visited[data[i].mid] = true;
             Union(data[i].id, data[i].mid);
+            exist[data[i].mid] = true;
         }
-        for(int j = 0; j < k; j++) {
+        for(int j = 0; j < data[i].k; j++) {
             scanf("%d", &data[i].cid[j]);
-            visited[data[i].cid[j]] = true;
             Union(data[i].id, data[i].cid[j]);
+            exist[data[i].cid[j]] = true;
         }
-        scanf("%d %d", &data[i].num, &data[i].area);
+        scanf("%lf %lf", &data[i].num, &data[i].area);
     }
-
-    for(int i = 0; i < n; i++) {        // 合并集
+    for(int i = 0; i < n; i++) {        // 统计
         int id = findFather(data[i].id);
-        ans[id].id = id;
-        ans[id].num += data[i].num;
-        ans[id].area += data[i].area;
-        ans[id].flag = true;
-    }
-
-    for(int i = 0; i < MAX; i++) {
-        if(visited[i]) {                // 统计每个家庭的人数
-            ans[findFather(i)].people++;
-        }
-        if(ans[i].flag) {               // 统计家庭数
+        family[id].id = id;
+        family[id].num += data[i].num;
+        family[id].area += data[i].area;
+        if(!family[id].visited) {
             cnt++;
         }
+        family[id].visited = true;
     }
-    for(int i = 0; i < MAX; i++) {
-        if(ans[i].flag) {
-            ans[i].num = (double)(ans[i].num * 1.0 / ans[i].people);        // 人均房产数
-            ans[i].area = (double)(ans[i].area * 1.0 / ans[i].people);      // 人均面积
+    for(int i = 0; i < MAX; i++) {      // 统计每个家庭人数
+        if(exist[i]) {
+            family[findFather(i)].k++;
         }
     }
-    sort(ans, ans + MAX, cmp);
-    printf("%d\n", cnt);                    // 输出
-    for(int i = 0; i < cnt; i++) {
-        printf("%04d %d %.3f %.3f\n", ans[i].id, ans[i].people, ans[i].num, ans[i].area);
+    sort(family, family + MAX, cmp1);
+    for(int i = 0; i < cnt; i++) {          // 计算
+        family[i].avgNum = double(family[i].num / family[i].k);
+        family[i].avgArea = double(family[i].area / family[i].k);
+    }
+    sort(family, family + cnt, cmp2);
+    printf("%d\n", cnt);
+    for(int i = 0; i < cnt; i++) {          // 输出
+        printf("%04d %d %.3lf %.3lf\n", family[i].id, family[i].k, family[i].avgNum, family[i].avgArea);
     }
     return 0;
 }
-
