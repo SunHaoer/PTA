3b8bf93c4674ac0a2e338fcc45e1d8025007c0d4
SunHao <2457302636@qq.com>
2018-09-19 19:54:03 +0800

    www.sunhao.pro

--- OJ_ZCMU/1061.cpp
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-int main()
-{
-    int a,b;
-    scanf("%d%d",&a,&b);
-    printf("The sum is %d\n",a+b);
-    printf("The difference is %d\n",a-b);
-    return 0;
-}
--- OJ_ZCMU/1066.cpp
+++
@@ -1,8 +0,0 @@
-#include<stdio.h>
-int main()
-{
-    char a;
-    a=getchar();
-    printf("%c",a+32);
-    return 0;
-}
